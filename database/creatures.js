6f99dbdf1a8613a70dbf7ef9b5390656b4a764c5
yvanicolas-droid <yvanicolas@gmail.com>
2026-07-19 23:56:00 +0200

    Update creatures.js

--- database/creatures.js
+++ database/creatures.js
@@ -1,1 +1,14 @@
-.
+const creatures = [
+  {
+    id: "CRE0001",
+    nom: "Salamandre de Feu",
+    element: "Feu",
+    biome: "Volcan",
+    meteo: "MET0001",
+    recompense: "RWD0001",
+    puissance: 20,
+    difficulte: 2,
+    description: "Une créature qui vit au cœur des volcans et se nourrit de la chaleur de la lave.",
+    image: "assets/images/creatures/salamandre-feu.png"
+  }
+];
