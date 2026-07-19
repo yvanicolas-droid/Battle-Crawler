ed678b1412e1e777d83555598c533b48f4fb4631
yvanicolas-droid <yvanicolas@gmail.com>
2026-07-19 23:57:01 +0200

    Update weather.js

--- database/weather.js
+++ database/weather.js
@@ -1,1 +1,8 @@
-.
+const weather = [
+  {
+    id: "MET0001",
+    nom: "Chaleur extrême",
+    effet: "+20 au score de combat des créatures de Feu.",
+    description: "Une chaleur intense favorise les créatures de type Feu et affaiblit les plus sensibles à la chaleur."
+  }
+];
