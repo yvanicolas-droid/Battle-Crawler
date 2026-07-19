b7b4d7262fe97319aab8ebaea88ad765d3923e48
yvanicolas-droid <yvanicolas@gmail.com>
2026-07-19 22:45:56 +0200

    Create card.js

---
+++ card.js
@@ -0,0 +1,14 @@
+const cards = [
+
+{
+id: "CRE0001",
+nom: "Salamandre de Feu",
+categorie: "Créature",
+element: "Feu",
+meteo: "Chaleur extrême",
+bonus: "+20",
+recompense: "Armure d'Écailles Incandescentes",
+image: "Creatures/images/salamandre-feu.png"
+}
+
+];
