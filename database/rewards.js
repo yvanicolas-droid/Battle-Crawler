6806fe521af0d7cafb7c3182100606709d61dc91
yvanicolas-droid <yvanicolas@gmail.com>
2026-07-19 23:57:42 +0200

    Update rewards.js

--- database/rewards.js
+++ database/rewards.js
@@ -1,1 +1,10 @@
-.
+const rewards = [
+  {
+    id: "RWD0001",
+    nom: "Armure d'Écailles Incandescentes",
+    type: "Armure",
+    rarete: "Rare",
+    bonus: "+20 Défense contre les attaques de Feu.",
+    description: "Une armure fabriquée à partir des écailles de la Salamandre de Feu."
+  }
+];
